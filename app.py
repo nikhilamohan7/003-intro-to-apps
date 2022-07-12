bd7ed103b9ac353be2b75d17abd540c5504d8c0e
Nikhila Mohan <jan.nikhila@gmail.com>
2022-07-12 22:59:26 +0000

    updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objs as go
 
 ########### Define your variables
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
+ibu_values=[40, 80, 90, 65]
 abv_values=[5.4, 7.1, 9.2, 4.3]
 color1='darkred'
 color2='orange'
