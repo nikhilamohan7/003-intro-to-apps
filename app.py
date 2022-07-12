c34c925555c4d96a26be04d4417cd51cbd13fff0
Nikhila Mohan <jan.nikhila@gmail.com>
2022-07-12 23:37:53 +0000

    updated

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ from dash import dcc
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[45, 70, 95, 100]
-abv_values=[5.4, 7.1, 9.2, 4.3]
+beers=['California', 'Texas', 'Florida', 'New York']
+ibu_values=[37.2, 25.1, 18.8, 19.3]
+abv_values=[39.2, 29.5, 21.7, 19.8]
 color1='blue'
-color2='orange'
-mytitle='Beer Comparison'
+color2='green'
+mytitle='Census population by State and Year'
 
-label1='IBU'
-label2='ABV'
+label1='2020 (in M)'
+label2='2021 (in M)'
 
 ########### Set up the chart
 
@@ -46,5 +46,5 @@ def make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytit
 
 if __name__ == '__main__':
     fig = make_that_cool_barchart(beers, ibu_values, abv_values, color1, color2, mytitle)
-    fig.write_html('docs/barchartnew.html')
+    fig.write_html('docs/barchartnew2.html')
     print('We successfully updated the barchart!')
